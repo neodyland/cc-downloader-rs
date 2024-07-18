af57d1d409ea5cb8bf6bf1ea74ad43483a4963dd
Googlefan <google@neody.dev>
2024-07-18 12:28:48 +0000

    feat: badwords

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ mod text;
 mod warc;
 
 use json::{object, stringify};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use tokio::{
     fs::{read_to_string, File},
     io::{AsyncWriteExt, BufWriter},
+    time::sleep,
 };
 
 #[tokio::main]
@@ -31,21 +32,26 @@ async fn main_inner() -> anyhow::Result<()> {
         .await?,
     );
     for a in all {
-        if let Ok(mut stream) = cc_stream::stream(a).await {
-            while let Some(s) = stream.recv().await {
-                f.write_all(
-                    stringify(object! {
-                        html: s,
-                    })
-                    .as_bytes(),
-                )
-                .await?;
-                f.write_all("\n".as_bytes()).await?;
-            }
-            println!("Success: {a}");
-        } else {
-            println!("Failed: {a}");
+        let mut stream = cc_stream::stream(a).await;
+        let mut attempt = 0;
+        while stream.is_err() {
+            println!("Attempt {attempt}");
+            sleep(Duration::from_secs(attempt)).await;
+            stream = cc_stream::stream(a).await;
+            attempt += 1;
         }
+        let mut stream = stream.unwrap();
+        while let Some(s) = stream.recv().await {
+            f.write_all(
+                stringify(object! {
+                    html: s,
+                })
+                .as_bytes(),
+            )
+            .await?;
+            f.write_all("\n".as_bytes()).await?;
+        }
+        println!("Success: {a}");
     }
     f.shutdown().await?;
     Ok(())
