8ff4b3f37c6fb067597b121f2fe9e4916624d6de
Googlefan <google@neody.dev>
2024-07-26 16:15:07 +0000

    feat: cc dl splitter

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,44 @@ mod gz_dec;
 mod text;
 mod warc;
 
-use async_compression::tokio::write::GzipEncoder;
-use json::{object, stringify};
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use arrow::{
+    array::{ArrayBuilder, RecordBatch, StringBuilder},
+    datatypes::{DataType, Field, Schema},
+};
+use parquet::arrow::AsyncArrowWriter;
+use parquet::basic::Compression;
+use parquet::basic::ZstdLevel;
+use parquet::file::properties::WriterProperties;
+use std::{sync::Arc, time::Duration};
 
 use tokio::{
     fs::{read_to_string, File},
-    io::{AsyncWriteExt, BufWriter},
     time::sleep,
 };
+
+async fn write_chunk_to_parquet(
+    string_builder: &StringBuilder,
+    file_counter: usize,
+) -> anyhow::Result<()> {
+    let schema = Schema::new(vec![Field::new("text", DataType::Utf8, false)]);
+
+    let batch = RecordBatch::try_new(
+        Arc::new(schema.clone()),
+        vec![Arc::new(string_builder.finish_cloned())],
+    )?;
+    let file = File::create(format!("output/part_{:05}.parquet", file_counter)).await?;
+
+    let props = WriterProperties::builder()
+        .set_compression(Compression::ZSTD(ZstdLevel::try_new(3).unwrap()))
+        .build();
+
+    let mut writer = AsyncArrowWriter::try_new(file, Arc::new(schema), Some(props))?;
+
+    writer.write(&batch).await?;
+    writer.close().await?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
@@ -25,13 +54,8 @@ async fn main_inner() -> anyhow::Result<()> {
     ft::get_model().await?;
     let all = read_to_string("./paths").await?.trim().to_string();
     let all = all.split('\n').collect::<Vec<_>>();
-    let mut f = GzipEncoder::new(BufWriter::new(
-        File::create(format!(
-            "output/{}.jsonl.gz",
-            SystemTime::now().duration_since(UNIX_EPOCH)?.as_micros()
-        ))
-        .await?,
-    ));
+    let mut string_builder = StringBuilder::new();
+    let mut file_counter = 0;
     for a in all {
         let mut stream = cc_stream::stream(a).await;
         let mut attempt = 0;
@@ -43,17 +67,17 @@ async fn main_inner() -> anyhow::Result<()> {
         }
         let mut stream = stream.unwrap();
         while let Some(s) = stream.recv().await {
-            f.write_all(
-                stringify(object! {
-                    html: s,
-                })
-                .as_bytes(),
-            )
-            .await?;
-            f.write_all("\n".as_bytes()).await?;
+            string_builder.append_value(s);
+            if string_builder.len() == 2500000 {
+                write_chunk_to_parquet(&string_builder, file_counter).await?;
+                string_builder = StringBuilder::new();
+                file_counter += 1;
+            }
         }
         println!("Success: {a}");
     }
-    f.shutdown().await?;
+    if !string_builder.is_empty() {
+        write_chunk_to_parquet(&string_builder, file_counter).await?;
+    }
     Ok(())
 }
