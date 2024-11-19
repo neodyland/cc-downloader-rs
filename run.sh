850423c5af0c1c54d669cb7ffd86b5c8005afda8
Googlefan <google@neody.dev>
2024-11-19 11:37:18 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-docker run -v ./docker/assets:/assets \
-    -v ./docker/output:/output \
+docker run -v ./docker/output:/output \
     -v ./docker/paths:/paths \
     -it --rm --name cc-dl \
     googlefan25/cc-dl
